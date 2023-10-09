eedbb6a89fd1525789328719543c4ada8c243ad2
Parsival1 <>
2023-10-09 15:00:24 -0500

    a
    a

--- Front-Demeter/src/pages/sale_detail_creation.jsx
+++ Front-Demeter/src/pages/sale_detail_creation.jsx
@@ -1,87 +1,118 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import {Link} from 'react-router-dom'
-import {useParams} from "react-router-dom"
-import '../sales_css/sales.css'
-import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from 'react-icons/ai';
+import '../sales_css/sales.css';  
 
-function Sale_detail_creation(updateTrigger ) {
-    let{id} = useParams()
-    let total = 0
-    const [List_Sales, setList_Sales] = useState([])
+function Sale_detail_creation({ updateTrigger }) {
+  const { id } = useParams();
+  const [listSales, setListSales] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
 
-    useEffect(() => {
-        axios.get(`http://localhost:5000/DetailWSale/${id}`).then((response) => {
-            setList_Sales(response.data)
-        })
-    }, [updateTrigger])
-    const ADD = async (id) => {
-        try {
-            await axios.put(`http://localhost:5000/updateDetail_ADD/${id}`);
-            
-        } catch (error) {
-            console.log(error);
-            console.log(id)
-        }
-    };
+  useEffect(() => {
+    fetchData();
+  }, [updateTrigger]);
 
-    const SubsTract = async (id) => {
-        try {
-            await axios.put(`http://localhost:5000/updateDetail_SUBSTRACT/${id}`);
-            
-        } catch (error) {
-            console.log(error);
-            console.log(id)
-        }
-    };
+  const fetchData = () => {
+    axios
+      .get(`http://localhost:5000/DetailWSale/${id}`)
+      .then((response) => {
+        setListSales(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  };
 
-    function getSale() {
-        List_Sales.map((value, key) => (
-            total += value.SubTotal
-        ))
-        return List_Sales.map((value, key) => (
-            <tr> 
-                <td scope="row">{value.ID_PRODUCTO}</td> 
-                <td scope="row" ><button className='add_cuantity' onClick={() => SubsTract(value.ID_DETALLE_VENTA)}><AiOutlineMinusCircle/></button>{value.Cantidad} <button className='subtract_cuantity' onClick={() => ADD(value.ID_DETALLE_VENTA)}><AiOutlinePlusCircle/></button> </td> 
-                <td scope='row'>{value.SubTotal}</td>    
-            </tr>
-        ))
+  const handleAdd = async (id) => {
+    try {
+      await axios.put(`http://localhost:5000/updateDetail_ADD/${id}`);
+      fetchData();
+    } catch (error) {
+      console.log('Error adding quantity:', error);
     }
+  };
 
-    
+  const handleSubtract = async (id) => {
+    try {
+      await axios.put(`http://localhost:5000/updateDetail_SUBSTRACT/${id}`);
+      fetchData();
+    } catch (error) {
+      console.log('Error subtracting quantity:', error);
+    }
+  };
 
-    return (
-        <div className='details_creation'>
-           <div className="back">
-           <Link to="/sales">
-           <button className="back-button">
-                 &#8592; Regresar
+  const calculateTotal = () => {
+    let sum = 0;
+    listSales.forEach((value) => {
+      sum += value.SubTotal;
+    });
+    return sum;
+  };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = listSales.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(listSales.length / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div className="details_creation">
+        
+      <div className="Title_detail">
+        <h1 className="sale-Title-Detail">Venta Numero {id}</h1>
+      </div>
+      <table className="table_detail">
+        <thead>
+          <tr>
+            <th scope="col">Producto</th>
+            <th scope="col">Cantidad</th>
+            <th scope="col">SubTotal</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems.map((value, key) => (
+            <tr key={key}>
+              <td scope="row">{value.ID_PRODUCTO}</td>
+              <td scope="row">
+                <button className="add_cuantity" onClick={() => handleSubtract(value.ID_DETALLE_VENTA)}>
+                  <AiOutlineMinusCircle />
+                </button>
+                {value.Cantidad}
+                <button className="subtract_cuantity" onClick={() => handleAdd(value.ID_DETALLE_VENTA)}>
+                  <AiOutlinePlusCircle />
+                </button>
+              </td>
+              <td scope="row">{value.SubTotal}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="pagination text-center mt-4 mx-100 ml-100">
+        <div className="pagination_c">
+          {pageNumbers.map((number) => (
+            <button
+              key={number}
+              onClick={() => paginate(number)}
+              className={`mx-1 px-3 py-1 rounded ${currentPage === number ? 'bg-orange-700 text-white' : 'bg-orange-400 hover:bg-orange-600 text-black'}`}
+            >
+              {number}
             </button>
-            </Link>
-           </div>
-            <div className='Title_detail'>
-                <h1 className='sale-Title-Detail'>Venta Numero {id}</h1>
-            </div>
-        <table className="table_detail">
-            <thead>
-                <tr>
-                    <th scope="col">Producto</th>
-                    <th scope="col">Cantidad</th>
-                    <th scope="col">SubTotal</th>
-                </tr>
-            </thead>
-            <tbody>
-                {getSale()} 
-            </tbody>
-        </table>
-            <div className="total">
-                <h1>Total: {total}</h1>
-            </div>
+          ))}
         </div>
-    )
-
-    
+      </div>
+      <div className="total">
+        <h1>Total: {calculateTotal()}</h1>
+      </div>
+    </div>
+  );
 }
-
 
 export default Sale_detail_creation;
